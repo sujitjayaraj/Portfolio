3da34fa1d190fc96315250fb714a9d42ed88eb39
Sujit Jayaraj <sujitjayaraj05@gmail.com>
2024-12-18 12:29:42 +0530

    Added resume and avatar as static resources
    Hosting resources on aws using s3 bucket costs public IPv4

--- app/utils/info/personal.js
+++ app/utils/info/personal.js
@@ -1,6 +1,6 @@
 export const personalInfo = {
     name: "Sujit Jayaraj",
-    image: "https://sujitjayaraj.s3.ap-south-1.amazonaws.com/Me.jpeg",
+    image: "/me.jpeg",
     designation: "Software Developer",
     description: "My name is Sujit Jayaraj, I am a professional and enthusiastic programmer with core skills in Java, JavaScript and Python. I love learning new technologies to build applications that improve the lives of people. I am a team player and I am always eager to learn from my peers and share my knowledge with them. Open to new opportunities and challenges that suits my skills and interests.",
     email: "sujitjayaraj23@gmail.com",
@@ -11,5 +11,5 @@ export const personalInfo = {
         "leetcode": "https://leetcode.com/u/sujitjayaraj235/",
         "twitter": "https://x.com/StunninglySujit"
     },
-    resume: "https://sujitjayaraj.s3.ap-south-1.amazonaws.com/Sujit+Jayaraj+resume.pdf"
+    resume: "/resume.pdf"
 }
