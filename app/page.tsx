fab5b85dc33a84aa8900d0b0f8f4593fd9cf7269
Sujit Jayaraj <sujitjayaraj05@gmail.com>
2025-03-04 20:59:44 +0530

    Implemented Skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/app/components/hero";
 import About from "@/app/components/about";
 import Experience from "@/app/components/experience";
+import Skills from "@/app/components/skills";
 import Education from "@/app/components/education";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
         <Hero />
         <About />
         <Experience />
+        <Skills />
         <Education />
       </div>
   )
