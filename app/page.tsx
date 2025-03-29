fe74d5961d8f88fee16f60899cee46232ed1d594
Sujit Jayaraj <sujitjayarajtech@gmail.com>
2025-03-29 13:14:27 +0530

    Implemented Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/app/components/hero";
 import About from "@/app/components/about";
 
 export default function Home() {
   return (
       <div>
+        <Hero />
         <About />
       </div>
   )
