ec2c8200d81f9693042bc63ae073154742fe2614
Sujit Jayaraj <sujitjayarajtech@gmail.com>
2025-03-29 13:36:32 +0530

    Implemented Projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/app/components/hero";
 import About from "@/app/components/about";
+import Projects from "@/app/components/projects";
 import Education from "@/app/components/education";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <div>
         <Hero />
         <About />
+        <Projects />
         <Education />
       </div>
   )
